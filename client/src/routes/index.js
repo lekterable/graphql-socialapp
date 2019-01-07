148fcad80a8c77020ee785880e48081fc7451824
Kornel Dubieniecki <lekterable@gmail.com>
2019-01-07 23:13:20 +0100

    Setup routing

---
+++ client/src/routes/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div>graphql-socialapp</div>
+			</Router>
+		)
+	}
+}
+
+export default App

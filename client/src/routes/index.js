b66741c6701f8fdf795cb7d01fd8bcaa8db68fdc
Kornel Dubieniecki <lekterable@gmail.com>
2019-01-09 23:25:55 +0100

    Add index.scss

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Home'
+import './index.scss'
 
 class App extends Component {
 	render() {

3a629feba4d7ee7b9e5f8d6e1866bab0ae906575
Kornel Dubieniecki <lekterable@gmail.com>
2019-01-10 23:53:05 +0100

    Add header.scss

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,20 @@
 import React from 'react'
 
+import './header.scss'
+
 export default () => {
-	return <div>Header</div>
+	return (
+		<div className="header">
+			<div className="header__left">
+				<div className="header__logo">graphql-socialapp</div>
+				<ul className="header__nav">
+					<li className="header__nav__item">Home</li>
+				</ul>
+			</div>
+			<div className="header__right">
+				<div className="header__register">Register</div>
+				<div className="header__login">Login</div>
+			</div>
+		</div>
+	)
 }

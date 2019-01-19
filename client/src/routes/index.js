4b17269720dbd3084ea881a32db7e63b47eaae84
Kornel Dubieniecki <lekterable@gmail.com>
2019-01-19 20:48:38 +0100

    Setup ApolloProvider

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import Header from '../components/Header'
@@ -6,18 +8,24 @@ import Footer from '../components/Footer'
 
 import './index.scss'
 
+const client = new ApolloClient({
+	uri: 'graphql'
+})
+
 class App extends Component {
 	render() {
 		return (
-			<Router>
-				<div className="container">
-					<Header />
-					<Switch>
-						<Route component={Home} />
-					</Switch>
-					<Footer content={'Made with ♥ by github.com/lekterable'} />
-				</div>
-			</Router>
+			<ApolloProvider client={client}>
+				<Router>
+					<div className="container">
+						<Header />
+						<Switch>
+							<Route component={Home} />
+						</Switch>
+						<Footer content={'Made with ♥ by github.com/lekterable'} />
+					</div>
+				</Router>
+			</ApolloProvider>
 		)
 	}
 }
